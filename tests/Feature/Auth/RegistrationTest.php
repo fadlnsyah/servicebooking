c6f57b1fabfddf31180ef3a13b288bca4468de9a
MFadlansy <fadlanzikri11@gmail.com>
2026-06-02 18:50:23 +0800

    Use MySQL test database

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -7,9 +7,12 @@ test('registration screen can be rendered', function () {
 });
 
 test('new users can register', function () {
+    $this->seed(\Database\Seeders\RolePermissionSeeder::class);
+
     $response = $this->post('/register', [
         'name' => 'Test User',
         'email' => 'test@example.com',
+        'phone' => '081234567890',
         'password' => 'password',
         'password_confirmation' => 'password',
     ]);
